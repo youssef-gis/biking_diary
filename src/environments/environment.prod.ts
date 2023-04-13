9b9ce827dc1639d35c874bfcdf481017a81bb629
YOUSSEF <40327134+youssef-gis@users.noreply.github.com>
2023-04-13 23:51:30 +0000

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  mapbox_api_key: "pk.eyJ1IjoieW91c3NlZmdpcyIsImEiOiJjbGc0aWRxa2owbGprM3BuMTA0cmJyaTB3In0.OxnhR6_xz83Cm7Rs-gZX7Q"
+ mapbox_api_key: "your_api_key"  
 };
